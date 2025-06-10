5201adb2304b162c437bbd287678a3c460fe9f5b
tomasagata <tomasagata00@gmail.com>
2025-06-10 12:54:23 +0200

    Added IML's CNI to oakestra's k8s deployments

--- oakestra-controller-manager/internal/controller/job_controller.go
+++ oakestra-controller-manager/internal/controller/job_controller.go
@@ -179,13 +179,15 @@ func (r *OakestraJobReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		}
 
 		deployment.Spec.Template.Annotations = map[string]string{
-			"k8s.v1.cni.cncf.io/networks": "oakestra-cni",
+			"k8s.v1.cni.cncf.io/networks": "[{\"name\": \"oakestra-cni\"},{\"name\":\"iml-cni\", \"namespace\":\"default\", \"cni-args\":{\"app_type\": \"application_function\", \"app_id\":\"" + oakestraJob.Spec.MicroserviceID + "\"}}]",
 			"oakestraPort":                oakestraJob.Spec.Port,
 		}
 
 		// replicaCount := int32(10)
 		// deployment.Spec.Replicas = &replicaCount
 		deployment.Spec.Template.Spec.RestartPolicy = corev1.RestartPolicyAlways
+
+		deployment.Spec.Template.Spec.NodeName = "mc-a7"
 
 		deployment.Spec.Template.Spec.Containers = []corev1.Container{
 			{
